506cd782553756861d0c12a41ad547acf2886e09
Damian Jacewicz <215739@edu.p.lodz.pl>
2020-06-24 22:21:18 +0200

    map

--- PG2D_2020_Dzienni_FD_Projekt/Game1.cs
+++ PG2D_2020_Dzienni_FD_Projekt/Game1.cs
@@ -61,8 +61,8 @@ namespace PG2D_2020_Dzienni_FD_Projekt
             // TODO: Add your initialization logic here
             tiledMap = new TiledMap(vResWidth, vResHeight);
 
-            int tileSpawnPointX = 150;
-            int tielSpawnPointY = 44;
+            int tileSpawnPointX = 41;
+            int tielSpawnPointY = 73;
             Player player = new Player(new Vector2(tileSpawnPointX * 32, tielSpawnPointY * 32), scripts);
 
             Vector2 realMapBeginning = new Vector2(tiledMap.tileSize * 31, tiledMap.tileSize * 31);
