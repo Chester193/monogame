cfb8987326f873782bd94e5494306eb98392c9ee
PrezesMajkel <prezesmajkel43@gmail.com>
2020-07-03 16:38:16 +0200

    trade 'q to quit'

--- PG2D_2020_Dzienni_FD_Projekt/States/TradeState.cs
+++ PG2D_2020_Dzienni_FD_Projekt/States/TradeState.cs
@@ -87,6 +87,8 @@ namespace PG2D_2020_Dzienni_FD_Projekt.States
 
             spriteBatch.DrawString(font, "Trader", new Vector2(730, 90), Color.Gold);
 
+            spriteBatch.DrawString(font, "Press Q to quit", new Vector2(40, 675), Color.White);
+
             foreach (var component in _components)
                 component.Draw(gameTime, spriteBatch);
 
